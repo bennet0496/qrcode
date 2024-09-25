7d4f1231609530a4db271e5b142e0dbed9914c40
Bennet Becker <bbecker@pks.mpg.de>
2024-09-25 11:23:41 +0200

    initial

---
+++ main.js
@@ -0,0 +1,25 @@
+import './style.css'
+import * as QRCode from 'qrcode'
+
+document.querySelector('#app').innerHTML = `<canvas id="canvas"></canvas>`
+
+const canvas = document.querySelector('canvas#canvas')
+
+const text = decodeURIComponent(window.location.hash.substring(1))
+
+console.log(QRCode.toCanvas);
+
+
+
+QRCode.toCanvas(canvas, text, {errorCorrectionLevel: 'H'}, function (error) {
+    if (error) {
+        console.error(error)
+        canvas.outerHTML = `failed to render QR Code`
+    }
+    console.log('success!');
+})
+
+const scaleTo = Math.min(window.innerHeight, window.innerWidth) * .5;
+
+canvas.style.width = scaleTo + "px";
+canvas.style.height = scaleTo + "px";
